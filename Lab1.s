209712c305fdd2bfc6dd780d8f92288f81c0a220
jwg0028 <jgodwin7148@gmail.com>
2024-02-02 21:29:19 -0600

    asd

--- Lab1.s
+++ Lab1.s
@@ -122,7 +122,7 @@ exit:
 .data
 
 .balign 4
-strHelloMessage: .asciz "Welcome to this array sum program. All arrays are 20 elements long. /nYou can enter the last 10 numbers for the second array by entering each number and pressing enter:\n"
+strHelloMessage: .asciz "Welcome to this array sum program. All arrays are 20 elements long. \nYou can enter the last 10 numbers for the second array by entering each number and pressing enter:\n"
 
 .balign 4
 array1: .word -10, -9, -8, -7,-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
