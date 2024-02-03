edb1c7eac1c0e913394a146274455347b8cd0561
jwg0028 <jgodwin7148@gmail.com>
2024-02-02 21:28:16 -0600

    asd

--- Lab1.s
+++ Lab1.s
@@ -21,6 +21,7 @@ r8: array1
 r9: array2
 r10: array3
 */
+
 .global main
 
 main:
@@ -36,12 +37,13 @@ main:
     ldr r8, =array1
     ldr r9, =array2
     ldr r10, =array3
-    @bl addingStart
     bl addingStart
     bl printStart
 
+    @Finish code
     b exit
 
+@Initializes for the userInputLoop
 getInput:
     push {r0, r1, r4, r9, lr}
     mov r4, #0
@@ -49,6 +51,7 @@ getInput:
 
 	@b userInputLoop
 
+@Loops taking user inputted numbers 10 times and stores them in the later half array2
 userInputLoop:
 	ldr r0, =fmtInt
 	ldr r1, =inputNum
@@ -64,12 +67,14 @@ userInputLoop:
 
 	pop {r0, r1, r4, r9, pc}
 
+@Initializes addingLoop
 addingStart:
 	push {r0, r1, r2, r4, r8, r9, r10, lr}
 	mov r4, #0
 	
 	@b addingLoop
 
+@Loops adding every element in array 1 and 2 and outputing that into array 3
 addingLoop:
 	ldr r1, [r8], #4
 	ldr r2, [r9], #4
@@ -83,6 +88,7 @@ addingLoop:
 
 	pop {r0, r1, r2, r4, r8, r9, r10, pc}
 
+@Initializes printStart
 printStart:
 	push {r0, r1, r2, r3, r4, r8, r9, r10, lr}
 	mov r4, #0
@@ -92,6 +98,7 @@ printStart:
 
 	@b printingLoop
 
+@Prints arrays 1-3 one element at a time
 printingLoop:
 	ldr r0, =strPrintArray
 	ldr r1, [r8], #4
@@ -110,16 +117,18 @@ exit:
     mov r0, #0x00
     svc 0
 
+
+@variable section
 .data
 
 .balign 4
-strHelloMessage: .asciz "Welcome to this array program. Enter 10 numbers for an array\n"
+strHelloMessage: .asciz "Welcome to this array sum program. All arrays are 20 elements long. /nYou can enter the last 10 numbers for the second array by entering each number and pressing enter:\n"
 
 .balign 4
 array1: .word -10, -9, -8, -7,-6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 
 .balign 4
-array2: .word 0, 2, 4, 6, 8, 10, 1, 3, 5, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+array2: .word 2, 4, 6, 8, 10, 1, 3, 5, 7, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 .balign 4
 array3: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
@@ -136,6 +145,7 @@ strFirstRow: .asciz "Array 1 | Array 2 | Array 3\n"
 .balign 4
 strPrintArray: .asciz "%d    +    %d    =    %d\n"
 
+@C library
 .global printf
 
 .global scanf
