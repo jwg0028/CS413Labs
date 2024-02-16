1df8fba9112c03fa9b26d5f932e03e0882b03b0a
jwg0028 <jgodwin7148@gmail.com>
2024-02-16 15:29:14 -0600

    asd

--- Lab2.s
+++ Lab2.s
@@ -29,23 +29,54 @@ main:
     b exit
 
 hexInput:
-    push {r0, r1}
+    push {r0, r1, lr}
     @Take user input
-    ldr r0, =fmtInt
-    ldr r1, =inputNum
+    ldr r0, =hexInput
+    ldr r1, =hexNum1
     bl scanf
 
+    ldr r8, [r1]
+    
+    push {r8}
+    
 
 hexInputCheck:
     @Check if input is valid
-
+    pop {r0, r1, pc}
 operationInput:
+
+    push {r0, r1, lr}
     @Take user input
+    ldr r0, =fmtInt
+	ldr r1, =intInput
+	bl scanf
+
+    ldr r1, intInput
+
+
+
+
 operationInputCheck:
     @Check if input is valid
     @Break to the corrosponding operation given from the input
+    
+    cmp r1, #1
+    beq andOperation
+
+    cmp r1, #2
+    cmp r1, #3
+
+    pop {r0, r1, pc}
 
 andOperation:
+    push {r8}
+    pop {r4}
+    ldr r9, =hexNum2
+
+    and r4, r9
+
+    pop {r0, r1, r4, r8, r9, pc}
+
 orrOperation:
 eorOperation:
 bicOperation:
@@ -70,7 +101,24 @@ strHexPrompt: .asciz "Enter Hex\n"
 strOperationPrompt: .asciz "Enter operation\n"
 
 .balign 4
+hexInput: .asciz "%x"
+
+.balign 4
+hexNum1: .word 0x0
+
+.balign 4
+hexNum2: .word 0xF0F0F0F0
+
+.balign 4
+operationInput: .word 0
+
+.balign 4
 fmtInt: .asciz "%d"
 
 .balign 4
-fmtInt: .ascii "0123456789ABCDEF"
+intInput: .word 0
+
+@C library
+.global printf
+
+.global scanf
