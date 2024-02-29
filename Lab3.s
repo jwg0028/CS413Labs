ede5ec0fc6608af7bff4f927425f9dcefd0e1f79
jwg0028 <jgodwin7148@gmail.com>
2024-02-29 15:31:27 -0600

    asd

---
+++ Lab3.s
@@ -0,0 +1,48 @@
+/*
+File: Lab_1.s
+Author: Jacob Wade Godwin
+
+Run command lines
+1) as -o Lab1.o Lab1.s -g && gcc -o Lab1 Lab1.o -g
+2) ./Lab1
+
+(I tried to name it Lab_1.s but I don't think the raspberry pi likes underscores)
+
+Debug lines
+3) gdb ./Lab1
+
+
+Registers used
+r0: General
+r1: General
+r2: General
+r3: General
+r4: Counter for loops
+r7: Used in exit function
+r8: array1
+r9: array2
+r10: array3
+*/
+.global main
+
+main:
+    @Welcomes the user on startup
+    ldr r0, =strHelloMessage
+    bl printf
+
+exit:
+    mov r7, #0x01
+    mov r0, #0x00
+    svc 0
+
+
+@variable section
+.data
+
+.balign 4
+strHelloMessage: .asciz "Welcome to Mr. Zippy's soft drink vending machine.\nCost of Coke, Sprite, Dr. Pepper, and Coke Zero is 55 cents.\n\nEnter money nickel (N), dime (D), quarter (Q), and one dollar bill (B).\n"
+
+@C library
+.global printf
+
+.global scanf
