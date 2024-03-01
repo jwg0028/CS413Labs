174d9d6ad196372b7b39950388d8d777bd40183f
jwg0028 <jgodwin7148@gmail.com>
2024-03-01 15:59:29 -0600

    asd

--- Lab3.s
+++ Lab3.s
@@ -1,7 +1,32 @@
+/*
+File: Lab_1.s
+Author: Jacob Wade Godwin
+
+Run command lines
+1) as -o Lab1.o Lab1.s -g && gcc -o Lab1 Lab1.o -g
+2) ./Lab1
+
+(I tried to name it Lab_1.s but I don't think the raspberry pi likes underscores)
+
+Debug lines
+3) gdb ./Lab1
+
+
+Registers used
+r0: General
+r1: General
+r2: General
+r3: General
+r4: Counter for loops
+r7: Used in exit function
+r8: array1
+r9: array2
+r10: array3
+*/
 .global main
 
 main:
-    @ Welcomes the user on startup
+    @Welcomes the user on startup
     ldr r0, =strHelloMessage
     bl printf
 
@@ -13,21 +38,31 @@ main:
     b exit
 
 takeInput:
-    push {r0, r1, r2, r3, r4, r5, lr}
+    push {r0, r1, r3, r4, lr}
+
+    ldr r5, =total
+
 
 inputLoop:
+
     ldr r0, =strYourTotal
-    ldr r1, =total
-    ldr r1, [r1]  @ Load the value of total
+    ldr r1, [r5]  @ Load the value of total
     bl printf
 
+
     ldr r0, =fmtChar
+	ldr r1, =inputChar
+	bl scanf
+
     ldr r1, =inputChar
-    bl scanf
 
-    ldrb r1, [inputChar]
 
-    @ section for branching based on coin inputted
+
+    @section for branching based on coin inputted
+
+    cmp r5, #55
+    bge breakLoop
+
     cmp r1, #'n'
     beq nickelCase
 
@@ -39,39 +74,42 @@ inputLoop:
 
     cmp r1, #'b'
     beq billCase
-
+    
     b inputLoop
 
-nickelCase:
-    ldr r2, =5
-    b addValueAndContinue
 
-dimeCase:
-    ldr r2, =10
-    b addValueAndContinue
+@section for breaking off and adding
+nickelCase:
+    add r5, r5, #5  @ Add nickel value to total
+    b inputLoop  @ Continue input loop
+
+
+dimeCase:  
+    add r5, r5, #10  @ Add dime value to total
+    b inputLoop  @ Continue input loop
 
 quarterCase:
-    ldr r2, =25
-    b addValueAndContinue
+    add r5, r5, #25  @ Add quarter value to total
+    b inputLoop  @ Continue input loop
+
 
 billCase:
-    ldr r2, =100
-    b addValueAndContinue
-
-addValueAndContinue:
-    ldr r3, =total
-    ldr r4, [r3]
-    add r4, r4, r2  @ Add coin value to total
-    str r4, [r3]  @ Store the updated total
+    add r5, r5, #100  @ Add bill value to total
     b inputLoop  @ Continue input loop
+
+
+breakLoop:
+    pop {r0, r1, r3, r4, r5, r6, r8, r9, pc}
 
 exit:
     mov r7, #0x01
     mov r0, #0x00
     svc 0
 
-@ variable section
+
+@variable section
 .data
+
 .balign 4
 strHelloMessage: .asciz "Welcome to Mr. Zippy's soft drink vending machine.\nCost of Coke, Sprite, Dr. Pepper, and Coke Zero is 55 cents.\n\n"
 
@@ -82,14 +120,18 @@ strInputLoop: .asciz "Enter money nickel (N), dime (D), quarter (Q), and one dol
 strYourTotal: .asciz "Your total is: %d\n"
 
 .balign 4
+strTest: .asciz "Test"
+
+.balign 4
 fmtChar: .asciz "%c"
 
 .balign 4
-inputChar: .space 1
+inputChar: .ascii 'a'
 
 .balign 4
 total: .word 0
 
-@ C library
+@C library
 .global printf
+
 .global scanf
